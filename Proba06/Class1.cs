eb1c4c1e3f63a021871358713323510e94862dbf
sergiosssg <sergiosssg@gmail.com>
2021-02-05 16:32:25 +0200

    Добавьте файлы проекта.

---
+++ Proba06/Class1.cs
@@ -0,0 +1,59 @@
+﻿using FirebirdSql.Data.FirebirdClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proba06
+{
+    
+    
+    class Class1
+    {
+        static private FbConnection fbConnection = null;
+        static private FbConnectionStringBuilder fb;
+
+        public static void InitDBConnection(string sServer, string sCharset, string sUserLogin, string sPassword, string sDB)
+        {
+            try
+            {
+
+                fb = new FbConnectionStringBuilder();
+
+                // devel2.frunze.local:sampd_cexs
+                fb.ConnectionString = sServer + ":" + sDB;
+                fb.Charset = sCharset;
+                fb.UserID = sUserLogin;
+                fb.Password = sPassword;
+
+                var fbconn = new FbConnection(fb.ToString());
+
+                Type typ = fbconn.GetType();
+
+
+                Console.WriteLine(typ.ToString());
+
+
+            }
+            catch (Exception ex)
+            {
+                Type tOfEx = ex.GetType();
+                Console.WriteLine("Error Type is {0}", tOfEx);
+                Console.WriteLine("============================");
+                Console.WriteLine("Error message is {0}", ex.Message);
+            }
+        }
+
+
+
+
+        static public FbConnection FirebirdConnection
+        {
+            get
+            {
+                return fbConnection;
+            }
+        }
+    }
+}
---
+++ Proba06/Entry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proba06
+{
+    class Entry
+    {
+        public static void Main(string[] sArgs )
+        {
+            string sGreeting = "Hello from proba";
+
+            Console.WriteLine("{0}", sGreeting);
+
+            // (string sServer, string sCharset, string sUserLogin, string sPassword, string sDB)
+            Class1.InitDBConnection("devel2.frunze.local:sampd_cexs", "WIN1251", "STAVITSKIY_S", "q1w2e3", "sampd_cexs");
+
+        }
+    }
+}
